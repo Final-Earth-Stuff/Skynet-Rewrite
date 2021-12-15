cef6f7eb1e789c44e7a3309780a4b84bb8f51184
TotallyNot <44345987+TotallyNot@users.noreply.github.com>
2021-12-15 23:45:02 +0100

    turn off synchronization

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,12 +2,12 @@ import { config } from "./src/config";
 
 export default {
     type: "postgres",
+    synchronize: false,
     host: config.databaseHost,
     port: config.databasePort,
     username: config.databaseUser,
     password: config.databasePassword,
     database: config.databaseName,
-    synchronize: true,
     entities: ["src/entity/**/*.ts"],
     migrations: ["src/migration/**/*.ts"],
 };
