60e22f67b0d88503a050a69c1f09e31c551b5d18
Natty_Boh <natty.boh.torn@gmail.com>
2022-04-07 12:27:06 -0400

    more bullying from prettier

--- src/repository/LandAndFacilitiesRespository.ts
+++ src/repository/LandAndFacilitiesRespository.ts
@@ -28,7 +28,9 @@ export class LandAndFacilitiesRepository extends Repository<LandAndFacilities> {
     getSpawnFactories(timestamp: Date) {
         return this.manager
             .createQueryBuilder(LandAndFacilities, "landAndFacilities")
-            .select("landAndFacilities.country, landAndFacilities.facs, MAX(landAndFacilities.timestamp)")
+            .select(
+                "landAndFacilities.country, landAndFacilities.facs, MAX(landAndFacilities.timestamp)"
+            )
             .where("landAndFacilities.timestamp > :timestamp", { timestamp })
             .andWhere("landAndFacilities.is_spawn = :isSpawn", {
                 isSpawn: true,
