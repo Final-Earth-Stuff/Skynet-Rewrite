4b11ac54bf81a2062582389adc14e3ddd837a78b
TotallyNot <44345987+TotallyNot@users.noreply.github.com>
2022-04-10 22:00:50 +0200

    implemented totals query

--- src/repository/LandAndFacilitiesRepository.ts
+++ src/repository/LandAndFacilitiesRepository.ts
@@ -1,5 +1,5 @@
 import { LandAndFacilities } from "../entity/LandAndFacilities";
-import { Country } from "../entity/Country";
+import { Country, Region } from "../entity/Country";
 
 import { AppDataSource } from "../";
 
@@ -8,6 +8,23 @@ export interface FacQueryRow {
     diff: number;
     team_control: number;
     name: string;
+}
+
+export interface TotalsQueryRow {
+    total_allies_facs: number;
+    total_axis_facs: number;
+    total_allies_rigs: number;
+    total_axis_rigs: number;
+    total_allies_mines: number;
+    total_axis_mines: number;
+
+    allies_capped: number;
+    axis_capped: number;
+    allies_uncapped: number;
+    axis_uncapped: number;
+
+    total_allies: number;
+    total_axis: number;
 }
 
 export const LandAndFacilitiesRepository = AppDataSource.getRepository(
@@ -58,6 +75,80 @@ export const LandAndFacilitiesRepository = AppDataSource.getRepository(
             .addSelect("current.team_control", "team_control")
             .addSelect("country.name", "name")
             .where("current.is_spawn");
+
+        return await query.getRawMany();
+    },
+
+    async totals(region?: Region) {
+        const lastFacilities = AppDataSource.createQueryBuilder()
+            .from("land_and_facilities", "laf")
+            .addSelect("laf.country", "country")
+            .addSelect("laf.facs", "facs")
+            .addSelect("laf.rigs", "rigs")
+            .addSelect("laf.mines", "mines")
+            .addSelect("laf.team_control", "control")
+            .distinctOn(["laf.country"])
+            .addOrderBy("laf.country")
+            .addOrderBy("laf.timestamp", "DESC");
+
+        const lastUnits = AppDataSource.createQueryBuilder()
+            .from("unit_change", "uc")
+            .addSelect("uc.allies", "allies")
+            .addSelect("uc.axis", "axis")
+            .addSelect("uc.country", "country")
+            .distinctOn(["uc.country"])
+            .addOrderBy("uc.country")
+            .addOrderBy("uc.timestamp", "DESC");
+
+        const query = AppDataSource.createQueryBuilder()
+            .addCommonTableExpression(lastFacilities, "lf")
+            .addCommonTableExpression(lastUnits, "lu")
+            .addSelect(
+                "sum(case lf.control when 0 then lf.facs end)",
+                "total_axis_facs"
+            )
+            .addSelect(
+                "sum(case lf.control when 100 then lf.facs end)",
+                "total_allies_facs"
+            )
+            .addSelect(
+                "sum(case lf.control when 0 then lf.mines end)",
+                "total_axis_mines"
+            )
+            .addSelect(
+                "sum(case lf.control when 100 then lf.mines end)",
+                "total_allies_mines"
+            )
+            .addSelect(
+                "sum(case lf.control when 0 then lf.rigs end)",
+                "total_axis_rigs"
+            )
+            .addSelect(
+                "sum(case lf.control when 100 then lf.rigs end)",
+                "total_allies_rigs"
+            )
+            .addSelect("sum(case lf.control when 0 then 1 end)", "axis_capped")
+            .addSelect(
+                "sum(case lf.control when 100 then 1 end)",
+                "allies_capped"
+            )
+            .addSelect(
+                "sum(case when lf.control between 1 and 49 then 1 end)",
+                "axis_uncapped"
+            )
+            .addSelect(
+                "sum(case when lf.control between 51 and 99 then 1 end)",
+                "allies_uncapped"
+            )
+            .addSelect("sum(lu.axis)", "total_axis")
+            .addSelect("sum(lu.allies)", "total_allies")
+            .from(Country, "country")
+            .innerJoin("lf", "lf", "country.id=lf.country")
+            .leftJoin("lu", "lu", "country.id=lu.country");
+
+        if (region) {
+            query.where("country.region=:region", { region });
+        }
 
         return await query.getRawMany();
     },
