43573e687645b0a2a55dc4466763919953a669c1
Natty_Boh <natty.boh.torn@gmail.com>
2022-06-05 13:14:32 -0400

    make start always ephemeral

---
+++ src/handler/global/help.ts
@@ -0,0 +1,28 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction, MessageEmbed } from "discord.js";
+
+import { CommandHandler, Command, CommandData, Guard } from "../../decorators";
+import { Color } from "../../service/util/constants";
+
+@CommandHandler({ name: "help" })
+export class Help {
+    @CommandData({ type: "global" })
+    readonly data = new SlashCommandBuilder()
+        .setName("help")
+        .setDescription("Help with bot usage")
+        .toJSON();
+
+    @Command()
+    async help(interaction: CommandInteraction): Promise<void> {
+        const success = new MessageEmbed()
+            .setTitle("Skynet Help")
+            .setDescription(`Please check out the Skynet [Wiki](https://github.com/Final-Earth-Stuff/Skynet-Rewrite/wiki) for more information.\n
+            Some useful links:
+            [Commands](https://github.com/Final-Earth-Stuff/Skynet-Rewrite/wiki/Commands)
+            [Setting up the bot in your server](https://github.com/Final-Earth-Stuff/Skynet-Rewrite/wiki/Setting-up-the-bot-in-your-server)
+            [FAQ](https://github.com/Final-Earth-Stuff/Skynet-Rewrite/wiki/FAQ)
+            `)
+            .setColor(Color.BLUE);
+        await interaction.reply({ embeds: [success] });
+    }
+}
