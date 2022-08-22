335647e4029924dd8cd6a04d91f84b2d6c938629
TotallyNot <44345987+TotallyNot@users.noreply.github.com>
2022-08-22 21:55:31 +0200

    fix snowflake column type

--- src/entity/Guild.ts
+++ src/entity/Guild.ts
@@ -2,33 +2,33 @@ import { Column, Entity, PrimaryColumn } from "typeorm";
 
 @Entity()
 export class Guild {
-    @PrimaryColumn({ type: "char", length: 19 })
+    @PrimaryColumn({ type: "varchar", length: 19 })
     guild_id!: string;
 
-    @Column({ type: "char", length: 19, nullable: true })
+    @Column({ type: "varchar", length: 19, nullable: true })
     allies_role?: string;
 
-    @Column({ type: "char", length: 19, nullable: true })
+    @Column({ type: "varchar", length: 19, nullable: true })
     axis_role?: string;
 
-    @Column({ type: "char", length: 19, nullable: true })
+    @Column({ type: "varchar", length: 19, nullable: true })
     verified_role?: string;
 
-    @Column({ type: "char", length: 19, nullable: true })
+    @Column({ type: "varchar", length: 19, nullable: true })
     spectator_role?: string;
 
-    @Column({ type: "char", length: 19, nullable: true })
+    @Column({ type: "varchar", length: 19, nullable: true })
     log_channel?: string;
 
-    @Column({ type: "char", length: 19, nullable: true })
+    @Column({ type: "varchar", length: 19, nullable: true })
     verify_channel?: string;
 
-    @Column({ type: "char", length: 19, array: true })
+    @Column({ type: "varchar", length: 19, array: true })
     command_channels!: string[];
 
-    @Column({ type: "char", length: 19, nullable: true })
+    @Column({ type: "varchar", length: 19, nullable: true })
     troop_movement_channel?: string;
 
-    @Column({ type: "char", length: 19, nullable: true })
+    @Column({ type: "varchar", length: 19, nullable: true })
     land_facility_channel?: string;
 }
--- src/entity/RankRole.ts
+++ src/entity/RankRole.ts
@@ -5,7 +5,7 @@ export class RankRole {
     @PrimaryGeneratedColumn()
     id!: string;
 
-    @Column({ type: "char", length: 19 })
+    @Column({ type: "varchar", length: 19 })
     guild_id?: string;
 
     @Column({ type: "int2", nullable: true })
