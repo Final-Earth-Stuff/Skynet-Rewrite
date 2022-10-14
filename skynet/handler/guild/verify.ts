cbef0fe503f0c12e2490f49072d7fb376643f75c
Natty_Boh <natty.boh.torn@gmail.com>
2022-10-14 16:19:37 -0400

    add verifying for other users

--- skynet/handler/guild/verify.ts
+++ skynet/handler/guild/verify.ts
@@ -25,6 +25,12 @@ export class Verify {
     readonly data = new SlashCommandBuilder()
         .setName("verify")
         .setDescription("Verify User")
+        .addUserOption((option) =>
+            option
+                .setName("user")
+                .setDescription("User to verify")
+                .setRequired(false)
+        )
         .toJSON();
 
     @Command()
@@ -33,19 +39,21 @@ export class Verify {
         if (!interaction.guild)
             throw new BotError("Command needs to be run in a guild");
 
+        const userId = interaction.options.getUser("user")?.id ?? interaction.user.id;
+
         const member = await interaction.guild.members.fetch(
-            interaction.user.id
+            userId
         );
         const guild = await getGuild(interaction.guildId ?? "");
 
         let user: UserData;
         try {
-            user = await ApiWrapper.bot.getUser(interaction.user.id);
+            user = await ApiWrapper.bot.getUser(userId);
         } catch (e) {
             if (e instanceof ApiError && e.code == 2) {
                 await resetMember(member, guild);
                 throw new BotError(
-                    `Your discord account is not verified with Final Earth.
+                    `This discord account is not verified with Final Earth.
                     Please visit [here](https://www.finalearth.com/discord) and follow the instructions.`
                 );
             } else {
@@ -68,10 +76,10 @@ export class Verify {
         const embed = new EmbedBuilder()
             .setAuthor({
                 name: user.name,
-                iconURL: interaction.user.displayAvatarURL(),
+                iconURL: member.user.displayAvatarURL(),
             })
             .setDescription(
-                `Successfully verified user ${interaction.user.tag}!`
+                `Successfully verified user ${member.user.tag}!`
             )
             .setColor(Color.GREEN);
 
